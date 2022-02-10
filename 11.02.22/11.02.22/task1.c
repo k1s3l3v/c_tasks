ad621996d90a1d3adb6ed586212f8e35f1e7a708
k1s3l3v <70627048+k1s3l3v@users.noreply.github.com>
2022-02-10 20:27:36 +0300

    Delete 11.02.22 directory

--- 11.02.22/11.02.22/task1.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-void main() {
-	int a, b, c, d, e;
-	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
-	printf("%f", (a + b + c + d + e) / (float)5);
-}
--- 11.02.22/task2/task2.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-void main() {
-    int i;
-    scanf("%i", &i);
-    if (i % 2 == 0) printf("even");
-    else printf("odd");
-}
